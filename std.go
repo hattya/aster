2ee79ef2c40ec5209180266f186f5afa3b999eef
Akinori Hattori <hattya@gmail.com>
2020-06-20 22:16:10 +0900

    Add SPDX IDs

--- std.go
+++ std.go
@@ -30,27 +30,9 @@ var files = map[string][]byte{
 	"language/go.js": []byte(`//
 // aster :: language/go.js
 //
-//   Copyright (c) 2017-2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2017-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
@@ -270,27 +252,9 @@ exports.packagesOf = function packagesOf(files) {
 	"language/javascript.js": []byte(`//
 // aster :: language/javascript.js
 //
-//   Copyright (c) 2017-2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2017-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
@@ -328,27 +292,9 @@ npm.test = function test() {
 	"language/python.js": []byte(`//
 // aster :: language/python.js
 //
-//   Copyright (c) 2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2018-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
@@ -401,27 +347,9 @@ coverage.xml = function xml() {
 	"language/restructuredtext.js": []byte(`//
 // aster :: language/restructuredtext.js
 //
-//   Copyright (c) 2017-2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2017-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
@@ -459,27 +387,9 @@ exports.rst2html = function rst2html(object) {
 	"language/vimscript.js": []byte(`//
 // aster :: language/vimscript.js
 //
-//   Copyright (c) 2017-2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2017-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
@@ -542,27 +452,9 @@ exports.themis = function themis() {
 	"language.js": []byte(`//
 // aster :: language.js
 //
-//   Copyright (c) 2017-2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2017-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
@@ -590,27 +482,9 @@ exports.system = function system(object) {
 	"os.js": []byte(`//
 // aster :: os.js
 //
-//   Copyright (c) 2014-2018 Akinori Hattori <hattya@gmail.com>
+//   Copyright (c) 2014-2020 Akinori Hattori <hattya@gmail.com>
 //
-//   Permission is hereby granted, free of charge, to any person
-//   obtaining a copy of this software and associated documentation files
-//   (the "Software"), to deal in the Software without restriction,
-//   including without limitation the rights to use, copy, modify, merge,
-//   publish, distribute, sublicense, and/or sell copies of the Software,
-//   and to permit persons to whom the Software is furnished to do so,
-//   subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be
-//   included in all copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-//   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-//   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//   SOFTWARE.
+//   SPDX-License-Identifier: MIT
 //
 
 'use strict';
